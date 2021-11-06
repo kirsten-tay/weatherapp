613adc5706b90d9d089c850f364495da0f4c7dc6
Kirsten tay <taykirsten11@gmail.com>
2021-11-06 23:46:54 +0000

    started a weather project

---
+++ screens/login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text, Image, TouchableOpacity, Pressable} from "react-native"
+import {AntDesign} from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function Login({navigation}) {
+    return(
+        <View style={{
+            flex:1,
+            justifyContent:"center", 
+            alignItems:"center", 
+            backgroundcolor: "white"
+            }}>
+            </View>
+
+    )
+        }
